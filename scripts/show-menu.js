424b4607f53bb0857e433c18a2f7cfb34905ea59
Dylan Salinas Palma <dylansalinaspalma@gmail.com>
2024-02-27 18:31:27 +0100

    27-02-2024--18-31

--- scripts/show-menu.js
+++ scripts/show-menu.js
@@ -3,6 +3,7 @@ function showMenu()
     let menuButton = document.querySelector(".mobile-menu-button");
     let openButton = document.querySelector(".open-menu");
     let closeButton = document.querySelector(".close-menu");
+    let menu = document.querySelector(".mobile-navigation-list");
 
     menuButton.onclick = () => 
     {
@@ -10,11 +11,14 @@ function showMenu()
         {
             openButton.style.opacity = "0";
             closeButton.style.opacity = "1";
+            menu.style.left = "0";
+
         }
         else
         {
             openButton.style.opacity = "1";
             closeButton.style.opacity = "0";
+            menu.style.left = "-100%";
         }
        
     }
